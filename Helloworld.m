418f67b1df59a7dc8c50a1f6ae008b52638e6cdc
Amnakhalid780 <73531231+Amnakhalid780@users.noreply.github.com>
2020-11-12 12:19:27 +0500

    Delete Helloworld.m

--- Helloworld.m
+++
@@ -1,4 +0,0 @@
-n = input('Enter a number:');
-for sentence = 1:n  % have the indexing go from 1 to n, not reverse
-    fprintf('%d. Hello world!\n', sentence); % print the index, and a newline \n
-end
